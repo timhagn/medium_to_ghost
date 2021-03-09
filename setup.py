54658a277bbd7c237e0466b4da2f242f13f4c560
Tim Hagn <mail@timhagn.com>
2021-03-09 19:57:43 +0100

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
 
 setup(
     name='medium_to_ghost',
-    version='0.0.4',
+    version='0.0.5',
     description="Convert a Medium blog export file to Ghost 2.0 format (including images)",
     long_description=readme,
     packages=['medium_to_ghost'],
