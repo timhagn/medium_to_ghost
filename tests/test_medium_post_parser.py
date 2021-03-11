43329d4a178b95a8fd11c1f7469757b4e99e0f0b
Tim Hagn <mail@timhagn.com>
2021-03-11 20:13:19 +0100

    fix test

--- tests/test_medium_post_parser.py
+++ tests/test_medium_post_parser.py
@@ -12,20 +12,20 @@ class TestMediumPostParser(unittest.TestCase):
 
         uuid, slug, date, status = medium_post_parser.parse_medium_filename(filename)
 
-        self.assertEquals(uuid, "f40359318721")
-        self.assertEquals(slug, "Machine-Learning-is-Fun--Part-3--Deep-Learning-and-Convolutional-Neural-Networks")
-        self.assertEquals(date, "2016-06-13")
-        self.assertEquals(status, "published")
+        self.assertEqual(uuid, "f40359318721")
+        self.assertEqual(slug, "Machine-Learning-is-Fun--Part-3--Deep-Learning-and-Convolutional-Neural-Networks")
+        self.assertEqual(date, "2016-06-13")
+        self.assertEqual(status, "published")
 
     def test_parse_medium_filename_draft(self):
         filename = "draft_test-7e48eb14931e.html"
 
         uuid, slug, date, status = medium_post_parser.parse_medium_filename(filename)
 
-        self.assertEquals(uuid, "7e48eb14931e")
-        self.assertEquals(slug, "test")
+        self.assertEqual(uuid, "7e48eb14931e")
+        self.assertEqual(slug, "test")
         self.assertIsNone(date)
-        self.assertEquals(status, "draft")
+        self.assertEqual(status, "draft")
 
     def test_MediumHTMLParser(self):
         doc = Path(os.path.join(os.path.dirname(__file__), 'test_data', 'draft_test-7e48eb14931e.html'))
@@ -39,19 +39,21 @@ class TestMediumPostParser(unittest.TestCase):
         mobiledoc = parser.convert()
 
         result = json.dumps(mobiledoc, indent=2)
-        self.assertEquals(result, expected_json)
+        self.assertEqual(result, expected_json)
 
     def test_convert_medium_post_to_ghost_json(self):
         doc = Path(os.path.join(os.path.dirname(__file__), 'test_data', 'draft_test-7e48eb14931e.html'))
         html = doc.read_text()
+        testuser = 1
 
         expected_mobiledoc = Path(os.path.join(os.path.dirname(__file__), 'test_data', 'draft_test_mobiledoc.json'))
         expected_json = expected_mobiledoc.read_text()
 
-        result = medium_post_parser.convert_medium_post_to_ghost_json("posts/draft_test-7e48eb14931e.html", html)
+        result = medium_post_parser.convert_medium_post_to_ghost_json("posts/draft_test-7e48eb14931e.html", html,
+                                                                      testuser)
 
-        self.assertEquals(result["uuid"], "7e48eb14931e")
-        self.assertEquals(result["title"], "Post Title")
-        self.assertEquals(result["slug"], "test")
-        self.assertEquals(result["status"], "draft")
-        self.assertEquals(result["mobiledoc"], expected_json)
+        self.assertEqual(result["uuid"], "7e48eb14931e")
+        self.assertEqual(result["title"], "Post Title")
+        self.assertEqual(result["slug"], "test")
+        self.assertEqual(result["status"], "draft")
+        self.assertEqual(result["mobiledoc"], expected_json)
