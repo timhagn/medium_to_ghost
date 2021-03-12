4f1f166803719410f9a5bc21f96c5e738a41b8f0
Tim Hagn <mail@timhagn.com>
2021-03-12 11:15:09 +0100

    switch from ids on cli to append option

--- medium_to_ghost/medium_additional_parser.py
+++ medium_to_ghost/medium_additional_parser.py
@@ -6,24 +6,24 @@ import re
 from medium_to_ghost.mtg_utils import download_with_local_cache
 
 
-def parse_tags(exported_posts, highest_tag_id):
+def parse_tags(exported_posts, existing_tags):
     """
     Parse a list of exported posts' canonical urls for Tags
     :param exported_posts: List of parsed Ghost posts.
-    :param highest_tag_id: List of parsed Ghost posts.
-    :return: Ghost versions of all tags & all post tags.
+    :param existing_tags: Already loaded tags or empty.
+    :return: Ghost versions of all tags & all post tags + all_tags combined.
     """
     all_posts_tags = []
-    all_tags = []
+    all_tags = existing_tags
 
     for post in exported_posts:
         if post["canonical_url"] is not None:
-            post_tags = parse_tags_from_canonical_url(post["canonical_url"], post["id"], all_tags, highest_tag_id)
+            post_tags = parse_tags_from_canonical_url(post["canonical_url"], post["id"], all_tags)
             if post_tags is not None:
                 all_posts_tags.extend(post_tags)
 
-    converted_tags = convert_all_tags(all_tags, highest_tag_id)
-    return converted_tags, all_posts_tags
+    converted_tags = convert_all_tags(all_tags)
+    return converted_tags, all_posts_tags, all_tags
 
 
 def post_filename_creator(uri):
@@ -33,13 +33,12 @@ def post_filename_creator(uri):
     return slug
 
 
-def parse_tags_from_canonical_url(canonical_url, post_id, all_tags, highest_tag_id):
+def parse_tags_from_canonical_url(canonical_url, post_id, all_tags):
     """
     Parse a canonical_url for Tags.
     :param canonical_url: Canonical URL of medium post.
     :param post_id: ID of POST as created.
     :param all_tags: List of all currently available tags.
-    :param highest_tag_id:
     :return: Ghost versions of all tags & all post tags.
     """
     post_tags = []
@@ -57,7 +56,7 @@ def parse_tags_from_canonical_url(canonical_url, post_id, all_tags, highest_tag_
             if anchor.contents[0] not in all_tags:
                 all_tags.append(anchor.contents[0])
             post_tag = {
-                "tag_id": all_tags.index(anchor.contents[0]) + highest_tag_id,
+                "tag_id": all_tags.index(anchor.contents[0]),
                 "post_id": post_id
             }
             post_tags.append(post_tag)
@@ -65,18 +64,17 @@ def parse_tags_from_canonical_url(canonical_url, post_id, all_tags, highest_tag_
     return post_tags
 
 
-def convert_all_tags(all_tags, highest_tag_id):
+def convert_all_tags(all_tags):
     """
     Converts all_tags to Ghost format.
     :param all_tags: List of all currently available tags.
-    :param highest_tag_id: .
     :return: Ghost versions of all_tags
     """
     converted_tags = []
     
     for tag_id, tag_name in enumerate(all_tags):
         ghost_tag = {
-            "id":           tag_id + highest_tag_id,
+            "id":           tag_id,
             "name":         tag_name,
             "description":  ""
         }
--- medium_to_ghost/medium_to_ghost.py
+++ medium_to_ghost/medium_to_ghost.py
@@ -47,6 +47,25 @@ def create_export_file(converted_posts, converted_tags, converted_posts_tags):
     }
 
 
+def update_export_file(json_output, converted_posts, converted_tags, converted_posts_tags):
+    """
+    Updates the existing Ghost import JSON, or falls back to create a new one.
+    :param json_output: Ghost formatted python docs.
+    :param converted_posts: Ghost formatted python docs.
+    :param converted_tags: Ghost formatted tags.
+    :param converted_posts_tags: Ghost formatted posts_tags.
+    :return: A Dict representation of a ghost export file you can dump to json.
+    """
+    if "db" in json_output:
+        json_output["db"][0]["data"]["posts"].extend(converted_posts)
+        json_output["db"][0]["data"]["tags"] = converted_tags
+        json_output["db"][0]["data"]["posts_tags"].extend(converted_posts_tags)
+    else:
+        logger.info(f"Export file not JSON compatible, creating new one!")
+        return create_export_file(converted_posts, converted_tags, converted_posts_tags)
+    return json_output
+
+
 def parse_posts(posts, user):
     """
     Parse a list of Medium HTML posts
@@ -83,45 +102,65 @@ def extract_posts_from_zip(medium_zip, highest_post_id):
     Extract all Medium posts from the Medium export Zip file as utf-8 strings
     :param medium_zip: zip file from Medium
     :param highest_post_id: ID of highest existing post
-    :return: list of posts as a dict with filename: (data, post_index)
+    :return: list of posts as a dict with filename: (data, post_index), post_index
     """
     posts = {}
+    post_index = 1
 
-    for post_index, filename in enumerate(medium_zip.namelist()):
+    for post_index, filename in enumerate(medium_zip.namelist(), start=1):
         if filename != "posts/" and filename.startswith("posts/"):
             data = extract_utf8_file_from_zip(medium_zip, filename)
-            posts[filename] = (data, post_index + highest_post_id)
+            posts[filename] = (data, post_index + highest_post_id + 1)
 
-    return posts
+    return posts, post_index + highest_post_id + 1
 
 
 @click.command()
 @click.option('-u', '--user', 'user', default=1, help='ID of user to set posts to.', show_default=True)
-@click.option('-hpid', '--highest_post_id', 'highest_post_id', default=1, help='ID of highest existing post.',
-              show_default=True)
-@click.option('-htid', '--highest_tag_id', 'highest_tag_id', default=1, help='ID of highest existing tag.',
-              show_default=True)
+@click.option('-a', '--append', 'append', is_flag=True, help='Shall new content be added to existing?')
 @click.argument('medium_export_zipfile')
-def main(medium_export_zipfile, user, highest_post_id, highest_tag_id):
+def main(medium_export_zipfile, user, append):
     export_folder = Path("exported_content")
     if Path(medium_export_zipfile).exists():
         export_folder.mkdir(parents=True, exist_ok=True)
+        export_file = "medium_export_for_ghost.json"
+        export_output = export_folder / export_file
+
+        existing_tags = []
+        highest_post_id = 1
+        json_output = {}
+
+        if append and export_output.exists():
+            try:
+                existing_output = open(export_output, "r")
+                json_output = json.load(existing_output)
+                existing_tags = json_output["existing_tags"]
+                highest_post_id = json_output["highest_post_id"]
+            except:
+                logger.info(f"Export file not JSON compatible, creating new one!")
+                append = False
+
         # TODO: instead of highest_post_id & highest_tag_id, cache tags & read them from JSON or such
         #       also perhaps read users from user import file & add them to individual exports...
-        with ZipFile(medium_export_zipfile) as medium_zip, open(export_folder / "medium_export_for_ghost.json",
-                                                                "w") as output:
-            # First extract the profile & parse the user.
+        with ZipFile(medium_export_zipfile) as medium_zip, open(export_output, "w") as output:
+            # TODO: First extract the profile & parse the user.
             raw_profile = extract_utf8_file_from_zip(medium_zip, 'profile/profile.html')
 
             # Next extract posts.
-            posts = extract_posts_from_zip(medium_zip, highest_post_id)
+            posts, post_index = extract_posts_from_zip(medium_zip, highest_post_id)
             exported_posts = parse_posts(posts, user)
 
             # Extract tags & align with posts.
-            exported_tags, exported_posts_tags = parse_tags(exported_posts, highest_tag_id)
+            # TODO: read existing tags if appending.
+            exported_tags, exported_posts_tags, all_tags = parse_tags(exported_posts, existing_tags)
 
-            # Finally create export file.
-            export_data = create_export_file(exported_posts, exported_tags, exported_posts_tags)
+            # Finally create or append export file.
+            if append and export_output.exists():
+                export_data = update_export_file(json_output, exported_posts, exported_tags, exported_posts_tags)
+            else:
+                export_data = create_export_file(exported_posts, exported_tags, exported_posts_tags)
+            export_data["highest_post_id"] = post_index
+            export_data["existing_tags"] = all_tags
             json.dump(export_data, output, indent=2)
 
         # Put everything in a zip file for Ghost
